f7f9855a4e0c5dc11e3a4abbd7f8fdd3a9b6783c
insua <insua@sohu.com>
2024-04-06 10:38:52 +0800

    :sparkles: (icon) update

--- format/icon.go
+++ format/icon.go
@@ -8,44 +8,27 @@ import (
 )
 
 func Icon(weather, sunrise, sunset string) string {
-	if funk.ContainsString([]string{"多云", "阴", "Dreary (Overcast)"}, weather) {
-		return ""
+	if funk.ContainsString([]string{"多云", "阴"}, weather) {
+		return "󮶳"
 	}
 	if weather == "Fog" {
-		return ""
+		return "󮳭"
 	}
-	if funk.ContainsString([]string{"晴", "Intermittent Clouds", "Mostly Sunny", "Partly Sunny", "Hazy Sunshine", "Hot"}, weather) {
-		sunriseArr := strings.Split(sunrise, ":")
-		sunsetArr := strings.Split(sunset, ":")
-		if len(sunriseArr) != 2 || len(sunsetArr) != 2 {
-			return ""
+	if funk.ContainsString([]string{"晴"}, weather) {
+		isNight := IsNight(sunrise, sunset)
+		if isNight == true {
+			return "󮴆"
 		}
-		sunriseHour := gconv.Uint(sunriseArr[0])
-		sunriseMinute := gconv.Uint(sunriseArr[1])
-		sunsetHour := gconv.Uint(sunsetArr[0])
-		sunsetMinute := gconv.Uint(sunsetArr[1])
-		now := gtime.Now()
-		nowHour := gconv.Uint(now.Format("G"))
-		nowMinute := gconv.Uint(now.Format("i"))
-		if nowHour == sunsetHour && nowMinute >= sunsetMinute {
-			return ""
-		}
-		if nowHour > sunsetHour {
-			return ""
-		}
-		if nowHour == sunriseHour && nowMinute <= sunriseMinute {
-			return ""
-		}
-		if nowHour < sunriseHour {
-			return ""
-		}
-		return ""
+		return "󮷓"
 	}
-	if funk.Contains([]string{"中雪", "小雪", "阵雪"}, weather) {
-		return ""
+	if funk.Contains([]string{"中雪", "阵雪"}, weather) {
+		return "󮣶"
+	}
+	if funk.Contains([]string{"小雪"}, weather) {
+		return "󮡷"
 	}
 	if funk.Contains([]string{"扬沙"}, weather) {
-		return ""
+		return "󮺭"
 	}
 	if weather == "Showers" {
 		return ""
@@ -54,7 +37,7 @@ func Icon(weather, sunrise, sunset string) string {
 		return ""
 	}
 	if funk.Contains([]string{"小雨"}, weather) {
-		return ""
+		return "󮣷"
 	}
 	if strings.Contains(weather, "Windy") {
 		return ""
@@ -81,4 +64,32 @@ func Icon(weather, sunrise, sunset string) string {
 		return ""
 	}
 	return weather
+}
+
+func IsNight(sunrise, sunset string) bool {
+	sunriseArr := strings.Split(sunrise, ":")
+	sunsetArr := strings.Split(sunset, ":")
+	if len(sunriseArr) != 2 || len(sunsetArr) != 2 {
+		return false
+	}
+	sunriseHour := gconv.Uint(sunriseArr[0])
+	sunriseMinute := gconv.Uint(sunriseArr[1])
+	sunsetHour := gconv.Uint(sunsetArr[0])
+	sunsetMinute := gconv.Uint(sunsetArr[1])
+	now := gtime.Now()
+	nowHour := gconv.Uint(now.Format("G"))
+	nowMinute := gconv.Uint(now.Format("i"))
+	if nowHour == sunsetHour && nowMinute >= sunsetMinute {
+		return true
+	}
+	if nowHour > sunsetHour {
+		return true
+	}
+	if nowHour == sunriseHour && nowMinute <= sunriseMinute {
+		return true
+	}
+	if nowHour < sunriseHour {
+		return true
+	}
+	return false
 }
